1490e44ded7daaa98396d62fcab9dd9c80415140
Marcin Kowalski <10373934u@qed.local>
2024-10-17 14:53:55 +0200

    first commit

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,45 +1,69 @@
-package com.example.statemanagementapp;
+package com.example.formvalidationapp;
 
 import android.os.Bundle;
+
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
-
 public class MainActivity extends AppCompatActivity {
-
-    private static final String KEY_COUNT = "count";
-    private TextView textViewCount;
-    private int count = 0;
+    private EditText editTextName;
+    private EditText editTextSurname;
+    private EditText editTextEmail;
+    private EditText editPhoneNumber;
+    private EditText editPassword;
+    private EditText editCheckPassword;
+    private Button buttonSubmit;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textViewCount= findViewById(R.id.textViewCount);
-        Button buttonIncrement = findViewById(R.id.buttonIncrement);
+        editTextName = findViewById(R.id.editTextname);
+        editTextSurname = findViewById(R.id.editTextSurname);
+        editTextEmail = findViewById(R.id.editTextEmail);
+        editPhoneNumber = findViewById(R.id.editPhonenumber);
+        editPassword = findViewById(R.id.editPassword);
+        editCheckPassword = findViewById(R.id.editCheckPassword);
+        buttonSubmit = findViewById(R.id.buttonSubmit);
 
-        if (savedInstanceState != null) {
-            count = savedInstanceState.getInt(KEY_COUNT);
-        }
-        updateCountText();
-
-        buttonIncrement.setOnClickListener(new View.OnClickListener() {
+        buttonSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(view v){
-                count ++;
-                updateCountText();
+            public void onClick(View v) {
+                String name = editTextName.getText().toString().trim();
+                String surname = editTextSurname.getText().toString().trim();
+                String email = editTextEmail.getText().toString().trim();
+                String phone = editPhoneNumber.getText().toString().trim();
+                String password = editPassword.getText().toString().trim();
+                String checkpassword = editCheckPassword.getText().toString().trim();
+
+                if (name.isEmpty()) {
+                    Toast.makeText(MainActivity.this, "wprowadź Imię", Toast.LENGTH_SHORT).show();
+                } else if (surname.isEmpty()) {
+                    Toast.makeText(MainActivity.this, "wprowadź nazwisko", Toast.LENGTH_SHORT).show();
+                } else if (email.isEmpty()) {
+                    Toast.makeText(MainActivity.this, "wprowadź adres e-mail", Toast.LENGTH_SHORT).show();
+                } else if (!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                    Toast.makeText(MainActivity.this, "Niepoprawny adres e-mail", Toast.LENGTH_SHORT).show();
+                } else if (phone.isEmpty()) {
+                    Toast.makeText(MainActivity.this, "wprowadź numer telefonu", Toast.LENGTH_SHORT).show();
+                } else if (phone.length() <9) {
+                    Toast.makeText(MainActivity.this, "Niepoprawny numer telefonu", Toast.LENGTH_SHORT).show();
+                } else if (password.isEmpty()) {
+                    Toast.makeText(MainActivity.this, "wprowadź hasło", Toast.LENGTH_SHORT).show();
+                } else if (password.length() <6) {
+                    Toast.makeText(MainActivity.this, "Hasło jest za krótkie. Wprowadz conajmniej 6 znaków", Toast.LENGTH_SHORT).show();
+                } else if (checkpassword.isEmpty()) {
+                    Toast.makeText(MainActivity.this, "wpisz hasło ponownie", Toast.LENGTH_SHORT).show();
+                } else if (!checkpassword.matches(password)) {
+                    Toast.makeText(MainActivity.this, "Hasła nie są takie same", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Formularz przesłany poprawnie", Toast.LENGTH_SHORT).show();
+                }
             }
         });
-    }
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        outState.putInt(KEY_COUNT, count);
-    }
-    private void updateCountText() {
-        textViewCount.setText() ("licznik: " + count);
     }
 }
